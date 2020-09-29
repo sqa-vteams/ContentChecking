75ced1852a6b7fa6d747b1f96bee9324bcc5cfba
ShahGitHub459 <61537965+ShahGitHub459@users.noreply.github.com>
2020-09-29 18:10:04 +0500

    Headless

--- ContentCheckingArtifact/src/main/java/DriverFactory/FireFoxDriverManager.java
+++ ContentCheckingArtifact/src/main/java/DriverFactory/FireFoxDriverManager.java
@@ -10,7 +10,9 @@ public class FireFoxDriverManager extends DriverManager{
 		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe"); 
 		System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
 		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-		FirefoxOptions options=new FirefoxOptions();
+		FirefoxOptions options = new FirefoxOptions();
+		options.setHeadless(true);
+		//FirefoxOptions options=new FirefoxOptions();
 		this.driver=new FirefoxDriver(options);
 	}
 
